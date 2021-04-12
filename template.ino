15037309ad223474b04aacdeea57fdccf6be4bdb
hoangdaoviet <77570106+hoangdaoviet@users.noreply.github.com>
2021-04-12 10:30:43 +0700

    Add files via upload

---
+++ template.ino
@@ -0,0 +1,30 @@
+//do not use this code
+#include "Servo.h"
+
+//define setup
+#define mode(x,y) pinMode(x,y)
+#define in INPUT
+#define out OUTPUT
+#define inpul INPUT_PULLUP
+
+//define special
+#define ss Serial
+#define servo Servo
+
+//define main code
+#define anaR(x) analogRead(x)
+#define digW(x,y) digitalWrite(x,y)
+#define high HIGH
+#define low LOW
+
+servo sunflower;
+#define sen A0
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
